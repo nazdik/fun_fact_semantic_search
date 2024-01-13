aefc35f58abd0eb1589b08704f410fad0817380f
Saeed <97466686+nazdik@users.noreply.github.com>
2024-01-13 22:44:00 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from txtai import Embeddings
+from db_facts import get_all_facts_text, get_fact_by_text, add_fact, delete_fact, update_fact
+import streamlit as st
+
+st.set_page_config(
+    page_title="Fun Facts",
+    page_icon="assets/favicon.png",
+    initial_sidebar_state="expanded", 
+)
+
+if 'embeddings' not in st.session_state:
+  st.session_state.embeddings = None
+
+if 'facts' not in st.session_state:
+    st.session_state.facts = get_all_facts_text()
+    st.session_state.embeddings = Embeddings({"path": "sentence-transformers/nli-mpnet-base-v2"})
+    st.session_state.embeddings.index(st.session_state.facts)
+
+# if 'fact_input' not in st.session_state:
+#     st.session_state['fact_input'] = ''
+
+
+
+
+
+st.title(":violet[Discover Fun Facts]")
+
+with st.form(key='query_form'):
+  query = st.text_input(label='What are you curious about?')
+  submit_button = st.form_submit_button(label='Find Fact')
+
+if submit_button and query != '' and st.session_state.embeddings is not None:
+    with st.spinner('Searching...'):
+      uid = st.session_state.embeddings.search(query,1)[0][0]
+      st.markdown(f'##### :star: {st.session_state.facts[uid]}')
+
+st.divider()
+
+def create_add_form():
+  with st.expander("What's Your Fun Fact?", expanded=False):
+    with st.form(key='add_form', clear_on_submit=True):
+      fact = st.text_input(label='Enter a fun fact', key='fact_input', )
+      submit_button = st.form_submit_button(label='Add Fact', )
+
+    if submit_button and fact != '':
+      if add_fact({"fact":fact}):
+        st.session_state.facts.append(fact)
+        st.session_state.embeddings.index(st.session_state.facts)
+        st.success('Fact added!')
+      else:
+        st.error('Error adding fact')
+
+
+create_add_form()
+
+
+
+    
+
---
+++ db_facts.py
@@ -0,0 +1,74 @@
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv('MONGO_URI')
+client = MongoClient(MONGO_URI)
+db = client['facts']
+
+facts_collection = db['facts']
+
+def get_all_facts():
+  try:
+    facts = facts_collection.find({},{"_id":0, "fact":1})
+    return facts
+  except: 
+    print('Error retrieving facts')
+    return []
+
+def get_all_facts_text():
+  try:
+    facts = facts_collection.find({},{"_id":0, "fact":1})
+    return [fact['fact'] for fact in facts]
+  except: 
+    print('Error retrieving facts')
+    return []
+
+def get_fact_by_id(id):
+  try:
+    fact = facts_collection.find_one({"id":id},{"_id":0, "fact":1})
+    return fact
+  except: 
+    print('Error retrieving fact')
+    return []
+
+def get_fact_by_text(text):
+  try:
+    fact = facts_collection.find_one({"fact":text},{"_id":0, "fact":1})
+    return fact
+  except: 
+    print('Error retrieving fact')
+    return []
+
+def add_fact(fact):
+  try:
+    facts_collection.insert_one(fact)
+    return True
+  except: 
+    print('Error adding fact')
+    return False
+
+def delete_fact(id):
+  try:
+    facts_collection.delete_one({"id":id})
+    return True
+  except: 
+    print('Error deleting fact')
+    return False
+
+def update_fact(id, fact):
+  try:
+    facts_collection.update_one({"id":id},{"$set":fact})
+    return True
+  except: 
+    print('Error updating fact')
+    return False
+
+ 
+
+# add_fact({ "fact":"'Dreamt' is the only English word that ends with the letters 'mt'."})
+
+
